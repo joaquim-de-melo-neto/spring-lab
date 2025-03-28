02c2c5080aa88c3227ec9789836905f258369c38
joaquim <joaquim01001@gmail.com>
2025-03-28 15:26:09 -0300

    update de LogginAspect.java

--- 22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
+++ 22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import rewards.internal.monitor.Monitor;
@@ -30,7 +31,7 @@ public class LoggingAspect {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private MonitorFactory monitorFactory;
 
-	
+	@Autowired
 	public LoggingAspect(MonitorFactory monitorFactory) {
 		super();
 		this.monitorFactory = monitorFactory;
